5a10fd1afcf1167024f8f679861498610cde6512
Rikela Smith <Rikela.Smith@bcbsnc.com>
2017-05-01 16:22:51 -0400

    050117v3

--- AddressBook/RecipesRepository.cs
+++ AddressBook/RecipesRepository.cs
@@ -4,7 +4,7 @@ using System.Data.SqlClient;
 
 namespace AddressBook
 {
-    public class RecipesRepository
+    public class RecipesRepository : IHandleRecipes
     {
         private string _connectionString;
 
